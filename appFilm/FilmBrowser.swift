2e69f2a64dea4c73f2391f0607b5d32782cd11e4
carlosrasensio <carlosrasensio@gmail.com>
2019-02-06 01:20:29 +0100

    Comienzo de la app. Se diseñan dos pantallas más el Navigation Controller. La primera es una tabla con un botón para añadir películas. La segunda es muy simple, introduces el nombre de la película en el TextField y pulsas un botón para añadirla a la tabla. Se utiliza CoreData.

---
+++ appFilm/FilmBrowser.swift
@@ -0,0 +1,44 @@
+//
+//  FilmBrowser.swift
+//  appFilm
+//
+//  Created by Carlos R. Asensio on 5/2/19.
+//  Copyright © 2019 Carlos R. Asensio. All rights reserved.
+//
+
+import UIKit
+
+class FilmBrowser: UIViewController {
+    
+    @IBOutlet var inputTextField: UITextField!
+    
+    @IBAction func addButton(_ sender: UIButton) {
+       /*
+        // Línea a continuación SIEMPRE en CoreData:
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext   // Se utiliza persistentContainer como vista de contexto y lo lleve todo al AppDelegate.
+        // Creamos la película:
+        let  film = Film(context: context)  // Se crea una película de tipo Película (entidad creada en iOSApp.xcdatamodeld).
+        film.name = inputTextField.text    // Al atributo name se le asigna el título de la película que se muestra en el inputtextField.
+        film.seen = false  // Al atributo seen se le asigna el valor por defecto false, es decir, que el usuario no ha visto la película.
+        // Se guardan los datos:
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()   // Función saveContext() está en el AppDelegate.
+        // Se envían esos datos a la otra pantalla:
+        navigationController?.popViewController(animated: true) // Se asigna a qué vista se envía el dato.
+        */
+        // Se utiliza persistentContainer como vista de contexto. Lo lleva todo al AppDelegate:
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        // Creamos la película:
+        let film = Film(context: context)
+        film.name = inputTextField.text // El atributo name lo sacamos del inputTextField.
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController(animated: true)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- appFilm/ViewController.swift
+++ appFilm/ViewController.swift
@@ -8,13 +8,82 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// Se añaden SIEMPRE los delegados UITableViewDataSource y UITableViewDelegate a la clase.
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet var filmTable: UITableView!
+    
+    var films : [Film] = [] // Variable films que está cogiendo los datos del array Film.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        filmTable.dataSource = self // La fuente de datos será ella misma.
+        filmTable.delegate = self   // El delegado será ella misma.
     }
-
+    
+    // Actualización de datos. La tabla se actualiza contínuamente:
+    override func viewWillAppear(_ animated: Bool) {
+        Data()
+        filmTable.reloadData()
+    }
+    
+    // Contador de películas hay almacenadas (función obligatoria):
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return films.count
+    }
+    
+    // Añadir celdas (función obligatoria):
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Se configuran las celdas:
+        let cell = UITableViewCell()    // Se asigna una celda a cada elemento.
+        let film = films[indexPath.row] // A cada película, que será una fila (row), se le va a asignar un indexPath.
+        cell.textLabel?.text = film.name! //A esa celda se le asignará un nombre.
+        
+        return cell
+    }
+    
+    // Trabajar con los datos:
+    func Data() {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do{
+            
+            films = try context.fetch(Film.fetchRequest())  // Hay que usarlo SIEMPRE. Significa que se envíe una respuesta de si los datos utilizados en AppDelegate los puede utilizar.
+            
+        } catch {
+            
+            print("Error en la función Data")
+            
+        }
+        
+    }
+    
+    // Borrar celdas:
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext   // Cada vez que se mete esta línea de código es para poder trabajar con estos datos.
+        
+        if editingStyle == .delete {    // Si le damos al botón de borrar.
+            
+            let film = films[indexPath.row]
+            context.delete(film)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext() // Se vuelven a guardar los datos.
+            
+            do{
+                
+                films = try context.fetch(Film.fetchRequest())  // Hay que usarlo SIEMPRE. Significa que se envíe una respuesta de si los datos utilizados en AppDelegate los puede utilizar. Es como una llamada al AppDelegate.
+                
+            } catch {
+                
+                print("Error en la función Borrar celdas")
+                
+            }
+        }
+        
+        filmTable.reloadData()
+    }
 
 }
 
