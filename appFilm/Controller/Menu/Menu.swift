37c3767299abf99e993d03a25fd7a11ad4f1068e
Carlos Rodriguez Asensio <croda@nae.es>
2019-09-20 19:42:59 +0200

    Added logic in Menu for handle the Auth button.

--- appFilm/Controller/Menu/Menu.swift
+++ appFilm/Controller/Menu/Menu.swift
@@ -10,18 +10,41 @@ import UIKit
 
 class Menu: UIViewController {
     
+    // MARK: - Outlets
+    @IBOutlet var authButton: UIBarButtonItem!
+    
     // MARK: - Constants
     let functions = Functions()
 
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let user = self.functions.readLoggedUser()
+        if user.signedIn {
+            authButton.title = "Logout"
+        } else {
+            authButton.title = "Login"
+        }
     }
 
     // MARK: - Firebase logout
-    @IBAction func logoutButtonPressed(_ sender: Any) {
-        let loggedUser = self.functions.readLoggedUser()
-        self.functions.userLogout(loggedUser: loggedUser)
+    @IBAction func authButtonPressed(_ sender: Any) {
+        let user = self.functions.readLoggedUser()
+        if user.signedIn {
+            self.functions.userLogout(loggedUser: user)
+            viewDidLoad()
+        } else {
+            self.goToScreen(storyboard: "Authenticate", screen: "rootAuthenticateStoryboard")
+        }
     }
+    
+    // MARK: - Navigation
+    func goToScreen(storyboard: String, screen: String) {
+        let storyboard = UIStoryboard(name: storyboard, bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: screen)
+        self.present(controller, animated: true, completion: nil)
+    }
+
+
 }
