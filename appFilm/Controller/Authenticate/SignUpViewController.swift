34da64da6eb459de0d0a71b2cc9065f29bcd8605
Carlos Rodriguez Asensio <croda@nae.es>
2019-09-20 21:40:15 +0200

    Modified style of Authenticate screens

--- appFilm/Controller/Authenticate/SignUpViewController.swift
+++ appFilm/Controller/Authenticate/SignUpViewController.swift
@@ -10,21 +10,17 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
     }
-    */
 
 }
